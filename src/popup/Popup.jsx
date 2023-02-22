c40d3bd04e6c278fe122dcebac4e3ed41007a4b5
lovexy-fun <lovexyfun@qq.com>
2023-02-23 01:34:43 +0800

    错误代码未删除问题

--- src/popup/Popup.jsx
+++ src/popup/Popup.jsx
@@ -3,7 +3,6 @@ import PopupStyle from "./Popup.scss";
 const hello = chrome.i18n.getMessage("popupHello");
 
 function Popup () {
-    a.
     return (
         <div className={ PopupStyle['hello-red'] }>{ hello }</div>
     );
